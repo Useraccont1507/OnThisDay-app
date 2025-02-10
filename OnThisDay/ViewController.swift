da3dcfa195575a1a251a9265e01c7445aa3e48b0
Illia Verezei <useraccont1507@gmail.com>
2025-02-10 10:38:49 +0200

    Initial Commit

---
+++ OnThisDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnThisDay
+//
+//  Created by Illia Verezei on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
